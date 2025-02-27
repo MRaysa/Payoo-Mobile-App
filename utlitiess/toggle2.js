cba60693c0fc8f44e64331e930cc1f0da615924e
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-02-27 14:55:35 +0600

    added traverse in every part

--- utlitiess/toggle2.js
+++ utlitiess/toggle2.js
@@ -1,22 +1,92 @@
 document.getElementById("cashout").style.display = "none";
+document.getElementById("addmoney").style.display = "none";
+document.getElementById("transferMoney").style.display = "none";
+document.getElementById("getBonus").style.display = "none";
+document.getElementById("payBill").style.display = "none";
+document.getElementById("TransactionHistory").style.display = "none";
 
-// for add money 
+//  add money 
 
 document.getElementById("add-money-box").addEventListener("click",
     function (){
         handleToggle("addmoney", "block");
         handleToggle("cashout","none");
+        handleToggle("transferMoney","none");
+        handleToggle("getBonus","none");
+        handleToggle("payBill","none");
         handleToggle("TransactionHistory","none");
+        handleToggle("LatestPayments","none");
     }
 )
 
-// for cash out
+//  cash out
 
 document.getElementById("cash-out-box").addEventListener("click",
     function (){
         handleToggle("cashout", "block");
         handleToggle("addmoney","none");
+        handleToggle("transferMoney","none");
+        handleToggle("getBonus","none");
+        handleToggle("payBill","none");
         handleToggle("TransactionHistory","none");
+        handleToggle("LatestPayments","none");
+    }
+)
+
+// transfer money
+
+document.getElementById("transfer-money-box").addEventListener("click",
+    function (){
+        handleToggle("cashout", "none");
+        handleToggle("addmoney","none");
+        handleToggle("transferMoney","block");
+        handleToggle("getBonus","none");
+        handleToggle("payBill","none");
+        handleToggle("TransactionHistory","none");
+        handleToggle("LatestPayments","none");
+    }
+)
+
+// get bonus 
+
+document.getElementById("get-bonus-box").addEventListener("click",
+    function (){
+        handleToggle("cashout", "none");
+        handleToggle("addmoney","none");
+        handleToggle("transferMoney","none");
+        handleToggle("getBonus","block");
+        handleToggle("payBill","none");
+        handleToggle("TransactionHistory","none");
+        handleToggle("LatestPayments","none");
+    }
+)
+
+//pay-bill-box
+
+document.getElementById("pay-bill-box").addEventListener("click",
+    function (){
+        handleToggle("cashout", "none");
+        handleToggle("addmoney","none");
+        handleToggle("transferMoney","none");
+        handleToggle("getBonus","none");
+        handleToggle("payBill","block");
+        handleToggle("TransactionHistory","none");
+        handleToggle("LatestPayments","none");
+    }
+)
+
+
+// Transaction History 
+
+document.getElementById("transactions-box").addEventListener("click",
+    function (){
+        handleToggle("cashout", "none");
+        handleToggle("addmoney","none");
+        handleToggle("transferMoney","none");
+        handleToggle("getBonus","none");
+        handleToggle("payBill","none");
+        handleToggle("TransactionHistory","block");
+        handleToggle("LatestPayments","none");
     }
 )
 
